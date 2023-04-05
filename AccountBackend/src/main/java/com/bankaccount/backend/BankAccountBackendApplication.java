6e2710bde6082615285c4987b96f0476b8aa31ac
Sulaiman1015 <sulaiman1015@gmail.com>
2023-04-05 13:40:09 +0200

    first init

---
+++ AccountBackend/src/main/java/com/bankaccount/backend/BankAccountBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bankaccount.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountBackendApplication.class, args);
+	}
+
+}
